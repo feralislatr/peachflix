01819e796f1e4a36b6bbc415b9d10ceae7f4a19d
feralislatr <12092944+feralislatr@users.noreply.github.com>
2024-09-06 11:53:24 -0400

    setup base layout and navigation - create home page layout - create app layout and base pages - add preliminary page styles - remove next default styles and fonts

---
+++ src/app/(movies)/layout.tsx
@@ -0,0 +1,31 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import styles from './styles.module.css';
+import appStyles from '../page.module.css';
+
+export default function MoviesLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className={styles.page}>
+      <header className={styles.header}>
+        <Link href={{ pathname: '/' }}>
+          <Image
+            className={appStyles.logo}
+            src="/logo.svg"
+            alt="peachflix"
+            width={159}
+            height={40}
+            priority
+          />
+        </Link>
+        <Link href={{ pathname: '/Movies' }}>Movies</Link>
+        <Link href={{ pathname: '/favorites' }}>Favorites</Link>
+        <input className={styles.input} type="text" placeholder="Search" />
+      </header>
+      {children}
+    </div>
+  );
+}
